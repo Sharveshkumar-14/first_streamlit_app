641c992f095eb40fa0bd663b9e14009831dfb828
Sharveshkumar-14 <55405945+Sharveshkumar-14@users.noreply.github.com>
2024-02-21 12:48:06 +0530

    Update streamlit_app.py
    typo fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.title('Breakfast Favourite')
 streamlit.header('🥣 Breakfat Menu')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞'Hard-boiled Free-Range Egg')
+streamlit.text('🥑🍞 Hard-boiled Free-Range Egg')
 
 streamlit.header('Build your own fruit smoothie')
 
