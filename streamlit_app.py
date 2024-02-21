d495b02527a6f430364d7941033fb4a6c9346073
Sharveshkumar-14 <55405945+Sharveshkumar-14@users.noreply.github.com>
2024-02-21 12:51:36 +0530

    Update streamlit_app.py
    added data frame from s3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title('Breakfast Favourite')
 
 
@@ -9,5 +10,10 @@ streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞 Hard-boiled Free-Range Egg')
 
 streamlit.header('Build your own fruit smoothie')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
+
 
 
